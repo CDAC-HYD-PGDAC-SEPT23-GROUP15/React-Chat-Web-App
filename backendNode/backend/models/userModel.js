94e608b76456fec8b7045d3ad70108e09038d706
Utkarsh Shrivastava <utkarshshrivastava.13@gmail.com>
2024-02-20 00:54:49 +0530

    Commit-06 [Refactor backend to Node.js]

--- backendNode/backend/models/userModel.js
+++ backendNode/backend/models/userModel.js
@@ -21,7 +21,7 @@ const userSchema = new Schema(
     pic: {
       type: String, // Assuming pic is stored as a URL
       default:
-        "https://cdn.vox-cdn.com/thumbor/LW8D1LdoptQEV-hgVmND-BlFYOk=/0x0:1920x1080/1220x813/filters:focal(1140x381:1446x687):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/67236826/batman_standalone_film_starring_and_directed_by_be_7xv2.0.jpg", // Default profile picture URL
+        "https://img.freepik.com/free-vector/isolated-young-handsome-man-different-poses-white-background-illustration_632498-859.jpg", // Default profile picture URL
     },
   },
   { timestamps: true }
--- backendSpringBoot/src/main/java/com/example/backendSpringBoot/repository/ChatRepository.java
+++ backendSpringBoot/src/main/java/com/example/backendSpringBoot/repository/ChatRepository.java
@@ -1,4 +1,3 @@
-// src/main/java/com/example/backendSpringBoot/repository/ChatRepository.java
 package com.example.backendSpringBoot.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
